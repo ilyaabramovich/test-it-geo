b10439c0763fb69efb3bc4a926b09b7d107ecdbc
ilyaabramovich <ilyaabramovich1998@gmail.com>
2019-10-03 18:21:44 +0300

    refactor and optimize soltion

--- TestItGeo/Program.cs
+++ TestItGeo/Program.cs
@@ -3,70 +3,83 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System;
-using System.Data;
 using System.Dynamic;
 using System.Text;
+using CsvHelper.Configuration;
+using System.Linq;
 
 namespace cshw
 {
     class Program
     {
+        public class Sign
+        {
+            public int Id { get; set; }
+            public double Lat { get; set; }
+            public double Lon { get; set; }
+            public double Weight { get; set; }
+            public int Panoid { get; set; }
+            public double Plat { get; set; }
+            public double Plon { get; set; }
+            public int Azimuth { get; set; }
+            public int Type { get; set; }
+        }
+
+        public class SignMap : ClassMap<Sign>
+        {
+            public SignMap()
+            {
+                AutoMap();
+                Map(m => m.Plat).Default(0);
+                Map(m => m.Plon).Default(0);
+            }
+        }
+
         const int R = 6371000;
         private static double GetDistance(double lat1, double lon1, double lat2, double lon2)
         {
-            //return R * Math.Acos(Math.Sin(lat1) * Math.Sin(lat2) + Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(lon2 - lon1));
             return R * 2 * Math.Asin(Math.Sqrt(Math.Pow(Math.Sin(Math.Abs(lat2 - lat1) * 0.5), 2) + Math.Cos(lat1) * Math.Cos(lat2) * Math.Pow(Math.Sin(Math.Abs(lon2 - lon1) * 0.5), 2)));
         }
 
-        private static bool IsDuplicate(DataRow r1, DataRow r2)
+        private static bool IsDuplicate(Sign s1, Sign s2)
         {
-            return (GetDistance(r1.Field<double>("lat"), r1.Field<double>("lon"), r2.Field<double>("lat"), r2.Field<double>("lon")) < 3) &&
-                (r2.Field<double>("weight") > r1.Field<double>("weight")) &&
-                (Math.Abs(r1.Field<int>("panoid") - r2.Field<int>("panoid")) > 8) &&
-                (Math.Abs(r1.Field<int>("azimuth") - r2.Field<int>("azimuth")) < 30) &&
-                (r1.Field<int>("type") == r2.Field<int>("type")) &&
-                (r1.Field<int>("id") != r2.Field<int>("id"));
+            return (s1.Type == s2.Type) &&
+                (GetDistance(s1.Lat, s1.Lon, s2.Lat, s2.Lon) < 3) &&
+                (Math.Abs(s1.Panoid - s2.Panoid) > 8) &&
+                (Math.Abs(s1.Azimuth - s2.Azimuth) < 30);
         }
 
         static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            var records = new List<dynamic>();
+            List<Sign> signs = new List<Sign>();
+            var duplicates = new List<dynamic>();
             using (var reader = new StreamReader("../../../input.csv", Encoding.GetEncoding(1251)))
             using (var csv = new CsvReader(reader))
             {
                 csv.Configuration.Delimiter = ";";
-                using (var dr = new CsvDataReader(csv))
+                csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();
+                csv.Configuration.CultureInfo = CultureInfo.InvariantCulture;
+                csv.Configuration.RegisterClassMap(new SignMap());
+                signs = csv.GetRecords<Sign>().ToList();
+            }
+            for (int i = 1; i < signs.Count; i++)
+            {
+                for (int j = 0; j < i; j++)
                 {
-                    var dt = new DataTable();
-                    dt.Locale = CultureInfo.InvariantCulture;
-                    dt.Columns.Add("id", typeof(int));
-                    dt.Columns.Add("lat", typeof(double));
-                    dt.Columns.Add("lon", typeof(double));
-                    dt.Columns.Add("weight", typeof(double));
-                    dt.Columns.Add("panoid", typeof(int));
-                    dt.Columns.Add("azimuth", typeof(int));
-                    dt.Columns.Add("type", typeof(int));
-                    dt.Load(dr);
-                    foreach (DataRow row1 in dt.Rows)
+                    if (IsDuplicate(signs[j], signs[i]))
                     {
-                        foreach (DataRow row2 in dt.Rows)
-                        {
-                            if (IsDuplicate(row1, row2))
-                            {
-                                dynamic record = new ExpandoObject();
-                                record.id = row1["id"];
-                                record.parentid = row2["id"];
-                                records.Add(record);
-                            };
-                        }
+                        dynamic record = new ExpandoObject();
+                        record.id = signs[j].Id;
+                        record.parentid = signs[i].Id;
+                        duplicates.Add(record);
                     }
                 }
             }
             using (var writer = new StreamWriter("../../../output.csv", false, Encoding.GetEncoding(1251)))
             using (var csv = new CsvWriter(writer))
             {
-                csv.WriteRecords(records);
+                csv.WriteRecords(duplicates);
             }
         }
     }
